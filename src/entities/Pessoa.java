90116af19268d6ab1916db3c2ce7aced6180fe38
Henry Tristão do Monte <henrytristao@gmail.com>
2026-05-14 08:38:31 -0300

    Adicionado classe entities

---
+++ src/entities/Pessoa.java
@@ -0,0 +1,95 @@
+package entities;
+
+public class Pessoa {
+
+	private String nome;
+	private String funcaoCasa;
+	private int idade;
+	private double peso;
+	private double altura;
+	private double inicioTrabalho;
+	private double fimTrabalho;
+	
+	public Pessoa() {
+		
+	}
+	
+	public Pessoa(String nome, String funcaoCasa, int idade, double peso, double altura, double inicioTrabalho,
+			double fimTrabalho) {
+		super();
+		this.nome = nome;
+		this.funcaoCasa = funcaoCasa;
+		this.idade = idade;
+		this.peso = peso;
+		this.altura = altura;
+		this.inicioTrabalho = inicioTrabalho;
+		this.fimTrabalho = fimTrabalho;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFuncaoCasa() {
+		return funcaoCasa;
+	}
+
+	public void setFuncaoCasa(String funcaoCasa) {
+		this.funcaoCasa = funcaoCasa;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double getInicioTrabalho() {
+		return inicioTrabalho;
+	}
+
+	public void setInicioTrabalho(double inicioTrabalho) {
+		this.inicioTrabalho = inicioTrabalho;
+	}
+
+	public double getFimTrabalho() {
+		return fimTrabalho;
+	}
+
+	public void setFimTrabalho(double fimTrabalho) {
+		this.fimTrabalho = fimTrabalho;
+	}
+
+	public double horasTrabalhada(double inicioTrabalho, double fimTrabalho) {
+		if (this.fimTrabalho >= this.inicioTrabalho) {
+			return this.fimTrabalho - this.inicioTrabalho;
+		} else {
+			return (24 - this.inicioTrabalho) + this.fimTrabalho;
+		}
+	}
+		
+	
+}
